4a26da2ba5e6806ea4452ac154fc62f789449a35
Delictum <31595759+Delictum@users.noreply.github.com>
2019-08-23 18:17:26 +0300

    Created common views & contollers

--- app/controllers/stories_controller.rb
+++
@@ -1,4 +0,0 @@
-class StoriesController < ApplicationController
-  def new
-  end
-end
--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,11 @@ class Task < ApplicationRecord
   validates :description, presence: true, length: { in: 2..300 }
   validates_format_of :status, with: /^([0-3])$/, :multiline => true
 
+  before_save :default_values
+  def default_values
+    self.status ||= 0 # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
+  end
+
   private
   def set_last_task
     last_task = self.as_json(include: [:comments, :user, :admin])
